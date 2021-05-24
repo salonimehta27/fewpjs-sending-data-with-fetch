53ff5fd412b799646250f61a714ee02cc2007f49
Saloni <salonimehta2127@gmail.com>
2021-05-24 00:16:52 -0400

    commit tree 8dae493942e8880c4a85b29b1a58f52977f27ef2

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData()
+{
+const formData={
+    name:"Steve",
+    email:"steve@steve.com"
+};
+const configObj={
+    method:"POST",
+    headers:{
+        "Content-Type":"application/json",
+        "Accept":"application/json"
+    },
+    body: JSON.stringify(formData)
+};
+return fetch('http://localhost:3000/users',configObj).
+then(resp=>resp.json()).
+then(object=> document.body.append(object.id))   
+.catch(error=>{
+    alert("Bad Thing")
+   document.body.append(error.message)})
+}
